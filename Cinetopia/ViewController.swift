b906ba4ce893b95b492af47eddd69842c32fcee5
Alexandre Henrique da Silva <echo.alexandre@gmail.com>
2024-10-26 09:31:03 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Alexandre Henrique da Silva on 26/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
